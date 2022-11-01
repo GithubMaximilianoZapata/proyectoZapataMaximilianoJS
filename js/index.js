cc1fe452011437252435ac32f207894e800dd2f7
Maximiliano Zapata <maximilianozapata84@gmail.com>
2022-11-01 15:18:47 -0300

    Clase,Objetos, Arrays,Metodos Array etc etc

--- js/index.js
+++ js/index.js
@@ -1,226 +1,146 @@
-                    
-
-/*DEFINICION DE VARIABLES PRECIO*/
-
-const precioTortaEvento = 500;
-
-const precioTortaAgasajo = 350;
-
-const precioTortaPostres = 250;
-
-const precioAlfajorChocolateNegro = 150;
-
-const precioMiniAlfajorChocolateNegro = 130;
-
-const precioAlfajorChocolatePremiun = 190;
-
-const precioMiniAlfajorChocolatePremiun = 140;
-
-const precioBombonChocolateNegro = 80;
-
-const precioBombonChocolatePremiun = 90;
-
-/*INICIAR VARIABLES PARA CICLO, SWITCH y FUNCIONES*/
-
+/// INICIALIZAR VARIABLES GLOBALES 
 let costoTotalCompra = 0;
-
 let flag;
+let total;
+let operacion;
+let opcion;
+let cantidad;
+// INSTANCIAR CLASE 
+class Delicatessen {
+    constructor(nombre, precio, descuento) {
+        this.nombre = nombre;       
+        this.precio = precio;
+        this.descuento = descuento;
+    };
+//METODOS
+    ventaArticulos (cantidad) {               
+        if (cantidad >= 12) {        
+            return total = costoTotalCompra += (parseFloat(this.precio * cantidad * this.descuento));    
+        } else {
+               return total =  costoTotalCompra +=  parseInt(cantidad * this.precio);    
+        }   
+    };    
+    salidaVenta (nombre, cantidad, precio, venta) {            
+        if (nombre == tortaEvento.nombre && cantidad < 12 || nombre == tortaAgasajo.nombre && cantidad < 12 || nombre == tortaPostre.nombre && cantidad < 12) { 
+            return(`${nombre} son ${cantidad} kg a $ ${precio} pesos el kilo; el total de su compra $ ${venta} pesos`); 
+        }        
+        else if (nombre == tortaEvento.nombre && cantidad >= 12 || nombre == tortaAgasajo.nombre && cantidad >= 12 || nombre == tortaPostre.nombre && cantidad >= 12) {
+            return(`${nombre} son ${cantidad} kg a $ ${precio} pesos el kilo menos descuento por cantidad; el total de su compra $ ${venta} pesos`); 
 
-let kg;
+        }        
+        else if (nombre != tortaEvento.nombre && cantidad <12 || nombre != tortaAgasajo.nombre && cantidad < 12 || nombre != tortaPostre.nombre && cantidad < 12) {
+            return(`${nombre} son ${cantidad} unidades a $ ${precio} pesos la unidad; el total de su compra $ ${venta} pesos`); 
+        } 
+        else {
+            return(`${nombre} son ${cantidad} unidades a $ ${precio} pesos la unidad menos descuento por cantidad; el total de su compra $ ${venta} pesos`); 
+        }
+    };    
+};
+//INSTANCIAMOS LOS OBJETOS Y GUARDAMOS EN UN ARRAY
+const mercaderia = [tortaEvento = new Delicatessen("1-Torta para Evento", 500, 0.90),
+ tortaAgasajo = new Delicatessen("2-Torta para Agasajo", 350, 0.95),
+ tortaPostre = new Delicatessen("3-Torta para Postres", 250, 0.97),
+ alfajorChocolateNegro = new Delicatessen("1-Alfajor Artesanal Chocolate Negro", 150, 0.95),
+ miniAlfajorChocolateNegro = new Delicatessen("2-Mini Alfajor de Chocolate Negro", 130, 0.97),
+ alfajorChocolatePremiun = new Delicatessen("3-Aljafor de Chocolate Premiun", 190, 0.90),
+ miniAlfajorChocolatePremiun = new Delicatessen("4-Mini Alfajor de Chocolate Premiun", 140, 0.95),
+ bombonesChocolateNegro = new Delicatessen("1-Bombones de Chocolate Negro", 80, 0.97),
+ bombonesChocolatePremiun = new Delicatessen("2-Bombones de Chocolate Premiun", 90, 0.95)];
+//METODOS DE ARRAY PARA VER OPCIONES
+const articulosNombre = mercaderia.map(articulo=>articulo.nombre);
+const verTortas = articulosNombre.slice(0,3);
+const verAlfajores = articulosNombre.slice(3,7);
+const verBombones = articulosNombre.slice(7,9);
+//MENU PARA QUE ELIJA EL CLIENTE
+const menu = ["1-Tortas", "2-Alfajores", "3-Bombones", "4-No deseo comprar"];
+//CARRITO DE COMRPAS
+const carritoCompra = [];
 
-let unidades;
-
-const descuentoPorDocena = 0.10;
-
-let precioConDescuento;
-
-/*FUNCIONES*/
-
-function porKilos (cantidad, precio) {
-
-    cantidad = kg;
-
-    total =  costoTotalCompra += (cantidad * precio);
-
-    console.log("El total de su compra es de: $ ", total);
-}
-
-
-function descuento (precio) {
-
-    let descuento = (precio * descuentoPorDocena);
-
-    let precioConDescuento = (precio - descuento);
-
-    return precioConDescuento;    
-}
-
-
-function porUnidades (cantidad, precio) {
-    
-    cantidad = unidades;
-
-    if (unidades >= 12) {             
-        
-        total = costoTotalCompra += (cantidad * precioConDescuento);
-
-        console.log("El total de su compra es de: $ ", total);
-
-    } else {
-            total = costoTotalCompra += (cantidad * precio);
-
-            console.log("El total de su compra es de: $ ", total);
-    } 
-}
-
-function mensajeKg (producto, precio) {
-
-    kg = parseInt(prompt("De cuantos Kg quiere su torta?"));
-
-    const compra = `${producto} ${kg} Kilos  a  $ ${precio} el kilo`;
-
-    console.log(compra);  
-
-}
-
-function mensajeUnidades (producto, precio) {
-
-    unidades = parseInt(prompt("Cuantas unidades desea comprar?"));
-    
-    precioConDescuento = descuento(precio);    
-
-    if (unidades < 12) {
-
-        const compra = `${producto} ${unidades} Unidades  a  $ ${precio} la unidad`;
-
-        console.log(compra); 
-    } 
-    
-    else {
-
-        const compra = `${producto} ${unidades} Unidades  a  $ ${precioConDescuento} la unidad a precio de descuento`;
-
-        console.log(compra);
-    }                  
-}
-
-
-/*CICLO DO WHILE CON CONDICIONALES, SWITCH Y FUNCIONES PARA SIMULAR LA COMPRA DE PRODUCTOS*/
-
+//CICLO DO WHILE PARA SIMULAR LA COMPRA DE PRODUCTOS
 do {
-    /*MENU DE PRODUCTOS PARA COMPRAR O CONSULTAR*/
-
-    const menu = parseInt(prompt(`Ingrese el número del articulo que desea comprar!!!: 
-    1- Torta para Evento
-    2- Torta para Agasajo
-    3- Torta para Postres
-    4- Alfajor Artesanal Chocolate Negro
-    5- Mini Alfajor de Chocolate Negro
-    6- Alfajor de chocolate Premiun
-    7- Mini Alfajor de Chocolate Premiun
-    8- Bombones de Chocolate Negro
-    9- Bombones de Chocolate Premiun
-    10- No deseo comprar nada
-    `));
-    
-    /*SI SOLO QUIERE CONSULTAR Y NO COMPRAR MARCANDO 10*/
-
-    if (menu == 10) {
-    alert("Muchas Gracias Por Su Visita!!!");
-    break; 
-    } 
-    
-    /* SI MARCA OPCION INCORRECTA (NO HEMOS VISTO TRATATIEMTO DE ERRORES) PREGUNTA SI DESEA COMPRAR*/
-  
-    else if (menu < 1 || menu > 10) {
-        alert(`Elija una opcion correcta del menu entre 1 y 9.
+    //MENU DE PRODUCTOS PARA COMPRAR O CONSULTAR
+    alert(`Elija lo que desea comprar: \n ${menu.join(" \n")}`);
+    const eleccion = parseInt(prompt(`Ingrese el número de opcion que desea`));           
+    //SI NO DESEA COMPRAR MARCANDO 4
+     if (eleccion == 4) {
+         alert(`Muchas Gracias Por Su Visita!!!`);
+    break;         
+    }     
+    //SI MARCA OPCION INCORRECTA (NO HEMOS VISTO TRATAMIENTO DE ERRORES) PREGUNTA SI DESEA COMPRAR O NO  
+    else if (eleccion < 1 || eleccion > 4) {
+        alert(`Elija una opcion correcta del menu entre 1 y 4.
         Presione "Y" para continuar su compra o "N" para Salir`);
     }
-
-    /*SI ELIGE ALGUN PRODUCTO DEL MENU ENTRA AL CICLO DE COMPRAS*/
-
-    else (menu >= 1 && menu <=9); { 
-    
-    /*SWITCH CON CADA PRODCUTO Y SU CASUISTICA*/
-
-    switch (menu) {
-
-        case (1):
-                                                    
-                    mensajeKg("Torta para Evento", precioTortaEvento);
-
-                    porKilos(kg, precioTortaEvento);
-                                         
-                    break;
-        case (2):
-                    
-                    mensajeKg("Torta para Agasajos", precioTortaAgasajo);
-                    
-                    porKilos(kg, precioTortaAgasajo);
-                                              
-                    break;
-        case (3):
-                    
-                    mensajeKg("Torta para Postres", precioTortaPostres);
-                    
-                    porKilos(kg, precioTortaPostres);
-                                                  
-                    break;
-        case (4):
-                    mensajeUnidades("Alfajor Artesanal Chocolate Negro", precioAlfajorChocolateNegro);                                  
-                                     
-                    porUnidades(unidades, precioAlfajorChocolateNegro);                    
-
-                    break;
-        case (5):
-                    mensajeUnidades("Mini Alfajor de Chocolate Negro", precioMiniAlfajorChocolateNegro);
-
-                    porUnidades(unidades, precioMiniAlfajorChocolateNegro);
-                                
-                    break;
-        case (6):
-                    mensajeUnidades("Alfajor de Chocolate Premiun", precioAlfajorChocolatePremiun);
-
-                    porUnidades(unidades, precioAlfajorChocolatePremiun);
-                                  
-                    break;
-        case (7):
-                    mensajeUnidades("Mini Alfajor de Chocolate Premiun", precioMiniAlfajorChocolatePremiun);
-
-                    porUnidades(unidades, precioMiniAlfajorChocolatePremiun);
-                                  
-                    break;
-        case (8):
-                    mensajeUnidades("Bombones de Chocolate Negro", precioBombonChocolateNegro);
-
-                    porUnidades(unidades, precioBombonChocolateNegro);
-                                  
-                    break;
-        case (9):
-                    mensajeUnidades("Bombones de Chocolate Premiun", precioBombonChocolatePremiun);
-
-                    porUnidades(unidades, precioBombonChocolatePremiun);
-                                  
-                    break;
-        default:
-            break;
-    };
-
-    /*REPREGUNTA AL USUARIO SI DESEA SEGUIR COMPRANDO O NO (CONTINUA CICLO O FINALIZA LA COMPRA)*/
-
+    //SI ELIGE ALGUN PRODUCTO DEL MENU ENTRA AL CICLO DE COMPRAS
+    else (eleccion >= 1 && eleccion <= 3); {             
+    //SWITCH CON CADA TIPO DE PRODUCTO Y SU CASUISTICA, ARMA ARRAY CARRITO DE COMPRA AL FINALIZAR LA COMPRA
+        switch (eleccion) {
+            case (1):
+                console.log(`Elija el articulo que desea comprar: \n ${verTortas.join("\n")}`);
+                opcion = parseInt(prompt(`Ingrese el numero de su articulo`));
+                cantidad = parseInt(prompt(`De Cuantos Kg Quiere Su Torta?`));
+                if (opcion == 1) {
+                venta = tortaEvento.ventaArticulos(cantidad);                
+                compra = tortaEvento.salidaVenta(tortaEvento.nombre, cantidad, tortaEvento.precio, venta.toFixed(2));
+                carritoCompra.push(compra);
+                }else if (opcion == 2){
+                    venta = tortaAgasajo.ventaArticulos(cantidad);
+                    compra = tortaAgasajo.salidaVenta(tortaAgasajo.nombre, cantidad, tortaAgasajo.precio, venta.toFixed(2));                  
+                    carritoCompra.push(compra);
+                } else {
+                    venta = tortaPostre.ventaArticulos(cantidad);
+                    compra = tortaPostre.salidaVenta(tortaPostre.nombre, cantidad, tortaPostre.precio,  venta.toFixed(2));
+                    carritoCompra.push(compra);   
+                };
+                break;
+            case (2):
+                console.log(`Elija el articulo que desea comprar: \n ${verAlfajores.join("\n")}`);
+                opcion = parseInt(prompt`Ingrese el numero de su articulo`);
+                cantidad =parseInt(prompt("Cuantas Unidades Desea Comprar?"))
+                if (opcion == 1) {
+                    venta = alfajorChocolateNegro.ventaArticulos(cantidad);
+                    compra = alfajorChocolateNegro.salidaVenta(alfajorChocolateNegro.nombre, cantidad, alfajorChocolateNegro.precio, venta.toFixed(2));    
+                    carritoCompra.push(compra); 
+                }else if (opcion == 2){
+                    venta = miniAlfajorChocolateNegro.ventaArticulos(cantidad);
+                    compra = miniAlfajorChocolateNegro.salidaVenta(miniAlfajorChocolateNegro.nombre, cantidad, miniAlfajorChocolateNegro.precio, venta.toFixed(2));    
+                    carritoCompra.push(compra);
+                } else if (opcion == 3) {
+                    venta = alfajorChocolatePremiun.ventaArticulos(cantidad);
+                    compra =alfajorChocolatePremiun.salidaVenta(alfajorChocolatePremiun.nombre, cantidad, alfajorChocolatePremiun.precio, venta.toFixed(2));    
+                    carritoCompra.push(compra); 
+                } else {
+                    venta = miniAlfajorChocolatePremiun.ventaArticulos(cantidad);
+                    compra = miniAlfajorChocolatePremiun.salidaVenta(miniAlfajorChocolatePremiun.nombre, cantidad, miniAlfajorChocolatePremiun.precio, venta.toFixed(2));    
+                    carritoCompra.push(compra); 
+                };
+                break;
+            case (3):
+                console.log(`Elija el articulo que desea comprar: \n ${verBombones.join("\n")}`);
+                opcion = parseInt(prompt`Ingrese el numero de su articulo`);
+                cantidad =parseInt(prompt("Cuantas Unidades Desea Comprar?"))
+                if (opcion == 1) {
+                    venta = bombonesChocolateNegro.ventaArticulos(cantidad);                
+                    compra = bombonesChocolateNegro.salidaVenta(bombonesChocolateNegro.nombre, cantidad, bombonesChocolateNegro.precio, venta.toFixed(2)); 
+                    carritoCompra.push(compra);
+                }else {
+                    venta = bombonesChocolatePremiun.ventaArticulos(cantidad);                
+                    compra = bombonesChocolatePremiun.salidaVenta(bombonesChocolatePremiun.nombre, cantidad, bombonesChocolatePremiun.precio, venta.toFixed(2)); 
+                    carritoCompra.push(compra);                
+                };                                                    
+                break;           
+            default:
+                break;            
+        };    
+    /*REPREGUNTA AL USUARIO SI DESEA SEGUIR COMPRANDO O NO CON METODO STRING UPPER PARA NO ROMPER CICLO
+    SI EL USUARIO INGRESA LA OPCION EN MINUSCULA
+    (CONTINUA CICLO O FINALIZA LA COMPRA MOSTRANDO EL ARRAY CARRITO DE COMPRA)*/
     flag = prompt("¿Desea continuar comprando? Y/N");
-        
-    }
-
-    /*WHILE CON METODO STRING POR SI USURIO INGRESA Y O N MINUSCULA NO ROMPA EL CICLO*/
-    
-} while (flag.toUpperCase() == "Y");
-
-
-
-
-
-        
-
+        if (flag.toLocaleUpperCase() == "N"){
+            console.log(carritoCompra.join(`\n`));
+        };
+    };         
+    //WHILE CON METODO STRING UPPER POR SI USURIO INGRESA Y O N MINUSCULA NO ROMPA EL CICLO    
+} while (flag.toUpperCase() == "Y");                    
 
 
